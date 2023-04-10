cb7f3d82f8e2c2b692c35aa61510163fb8ded82a
lukedeakinbell <130125654+lukedeakinbell@users.noreply.github.com>
2023-04-10 18:00:44 +1000

    Update Scrapetweetscode.py

--- Scrapetweetscode.py
+++ Scrapetweetscode.py
@@ -1,22 +1,26 @@
 import tweepy
 
-# Twitter API credentials
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_secret = 'your_access_secret'
+# Set up your Twitter API credentials
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
 
 # Authenticate with the Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Search for tweets mentioning Bitcoin
-query = 'Bitcoin'
-tweets = api.search(q=query, lang='en', count=100)
+# Define your search query and the number of tweets you want to retrieve
+search_query = "bitcoin OR BTC"
+num_tweets = 100
+
+# Collect tweets using the search query
+tweets = tweepy.Cursor(api.search_tweets,
+                       q=search_query,
+                       lang="en",
+                       tweet_mode="extended").items(num_tweets)
 
 # Print the text of each tweet
 for tweet in tweets:
-    print(tweet.text)
-
-    #example for an additional change to test the branch function. 
+    print(tweet.full_text)
